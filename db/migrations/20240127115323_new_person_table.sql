f048b48339b6bf7d08c45aed320f5d8ecf3af153
Valery <korolevvalery37@gmail.com>
2024-01-27 18:15:44 +0600

    create goose migration

---
+++ db/migrations/20240127115323_new_person_table.sql
@@ -0,0 +1,16 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS persons (
+    name VARCHAR(255) NOT NULL,
+    surname VARCHAR(255) NOT NULL,
+    patronymic VARCHAR(255),
+    age SMALLINT NOT NULL,
+    nationality VARCHAR(255) NOT NULL,
+    sex VARCHAR(7) NOT NULL
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS persons;
+-- +goose StatementEnd
